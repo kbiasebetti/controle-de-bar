b041b26aa135233ad7132722b17aac72ff12fbdb
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-06-26 19:22:23 -0300

    Implementado fechamento de contas

--- ControleDeBar.ConsoleApp/ModuloConta/TelaConta.cs
+++ ControleDeBar.ConsoleApp/ModuloConta/TelaConta.cs
@@ -30,8 +30,9 @@ public class TelaConta : ITela
         ExibirCabecalho();
 
         Console.WriteLine($"1 - Cadastro de Conta");
-        Console.WriteLine($"2 - Gerênciar Pedidos da Conta");
-        Console.WriteLine($"3 - Visualizar Contas");
+        Console.WriteLine($"2 - Fechamento de Conta");
+        Console.WriteLine($"3 - Gerênciar Pedidos da Conta");
+        Console.WriteLine($"4 - Visualizar Contas");
         Console.WriteLine($"S - Sair");
 
         Console.WriteLine();
@@ -117,6 +118,33 @@ public class TelaConta : ITela
 
     public void EditarRegistro()
     {
+        ExibirCabecalho();
+
+        Console.WriteLine("Fechamento de Conta");
+
+        Console.WriteLine();
+
+        VisualizarRegistros(false);
+
+        Console.Write("Digite o ID da conta que deseja fechar: ");
+        int id = Convert.ToInt32(Console.ReadLine());
+
+        Conta contaSelecionada = repositorioConta.SelecionarContaPorId(id);
+
+        Console.WriteLine();
+
+        Console.Write($"Deseja realmente fechar a conta do titular \"{contaSelecionada.Titular}\"? (s/N)");
+        char opcaoEscolhida = Console.ReadLine()[0];
+
+        if (char.ToUpper(opcaoEscolhida) == 'N')
+            return;
+
+        contaSelecionada.Fechar();
+
+        ApresentarMensagem(
+            $"Conta do titular \"{contaSelecionada.Titular}\" fechada com sucesso!",
+            ConsoleColor.Green
+        );
     }
 
     public void ExcluirRegistro()
