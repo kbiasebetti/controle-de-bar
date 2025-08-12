799be246324b8b0b6c59b00a11d6a791ec968403
kbiasebetti <kauamengel17@gmail.com>
2025-08-12 20:33:39 -0300

    Implementado página de detalhes da conta e inicio de resposividade

---
+++ ControleDeBar.WebApp/Views/Conta/.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ControleDeBar.WebApp.Views.Conta
+{
+    public class DetalhesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
